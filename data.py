b8c155fad04d97ceecebc975eec5d9e64072d866
Phellipsor <ragnarok.kosyachenko@gmail.com>
2024-01-15 20:30:30 +0200

    Added daily news

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import calendar
 
-def get_news():
+def week_news():
     response = requests.get("https://nfs.faireconomy.media/ff_calendar_thisweek.json?version=4e7b65142501ec6ffc7d6c68048d2fc7")
     data = response.content.decode("utf8").replace("'", '"')
     list_data = json.loads(data)
@@ -17,11 +17,35 @@ def get_news():
             weekday = calendar.day_name[date.weekday()]
             title = e["title"]
             country = e["country"]
-            if e["impact"] == "High":
-                emoji = "🔴"
-            else:
-                emoji = "🟠"
+            emoji = "🔴"
             text = f"{time} {weekday} {date} \n{country} {emoji} \n{title}\n"
             formatted_news.append(text)
-    print(formatted_news)
+
     return formatted_news
+    
+    
+def daily_news():
+    response = requests.get("https://nfs.faireconomy.media/ff_calendar_thisweek.json?version=4e7b65142501ec6ffc7d6c68048d2fc7")
+    data = response.content.decode("utf8").replace("'", '"')
+    list_data = json.loads(data)
+    
+    formatted_news = []
+    
+    today = datetime.now().date()
+    weekday = calendar.day_name[today.weekday()]
+    
+    for e in list_data:
+        if e["impact"] == "High" or e["impact"] == "Medium":
+                if e["date"][0:10] == str(today):
+                    time = e["date"][11:16]
+                    title = e["title"]
+                    country = e["country"]
+                    if e["impact"] == "High":
+                        emoji = "🔴"
+                    else:
+                        emoji = "🟠"
+                    text = f"{emoji} {country} {time}\n{title}"
+                    formatted_news.append(text)
+    
+    return formatted_news
+    
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import schedule
 from time import sleep
 from threading import Thread
-from telebot import TeleBot, types
-from data import get_news
+from telebot import TeleBot
+from data import week_news, daily_news
 
 bot = TeleBot("6607124610:AAF0rMkhABJvWl5x2Hs7g-_oz8UVzPpCRvk")
-zheka = 353616677
-
-@bot.message_handler(commands=["start"])
-def start(msg):
-    bot.send_message(msg.chat.id, "Отримаєш звіт на тиждень о 8 ранку понеділка.")
+users = [353616677, 382267161]
 
 def schedule_checker():
     while True:
         schedule.run_pending()
         sleep(1)
         
-def func_to_run():
-    news = f"{'-'*35}\n".join(get_news())
-    bot.send_message(zheka, news)
+def weekly():
+    news = "News for next week:\n\n" + f"{'-'*35}\n".join(week_news())
+    for us in users:
+        bot.send_message(us, news)
+    
+def daily():
+    news_list = daily_news()
+    news = "News for today:\n\n" + f"{'-'*35}\n".join(news_list)
+    for us in users:
+        if len(news_list) > 0:
+            bot.send_message(us, news)
+        else:
+            bot.send_message(us, "No important news for today")
         
 if __name__ == "__main__":
-    schedule.every().monday.at("08:00", "Europe/Kyiv").do(func_to_run)
+    schedule.every().monday.at("08:00", "Europe/Kyiv").do(weekly)
+    schedule.every().day.at("08:00", "Europe/Kyiv").do(daily)
     Thread(target=schedule_checker).start()
-
+    
 bot.infinity_polling()
